83a57c33c1baf6fabe8199a90256f43ca5b67ef6
lucashmsilva <lucashenrique580@gmail.com>
2025-02-28 20:25:48 -0300

    0-smoke-test solution iteration

--- 0-smoke-test/main.go
+++ 0-smoke-test/main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
-	fmt.Println("oi")
+	ln, err := net.Listen("tcp", "66.228.43.192:6767")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Accept incoming connections and handle them
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		// Handle the connection in a new goroutine
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	// Close the connection when we're done
+	defer conn.Close()
+
+	// Read incoming data
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the incoming data
+	fmt.Printf("Received: %s", buf)
+	conn.Write(buf)
 }
